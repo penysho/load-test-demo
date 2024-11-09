d1f2ea1287b1f88c44b3e930d8c84f2e196b0fa6
penysho <penysho@gmail.com>
2024-11-10 00:47:10 +0900

    feat: axum導入

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8011").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, world!</h1>")
 }
